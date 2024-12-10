3c8509fec3074427ccd454005eca3b63d8517ddc
rodrigosv21 <rodrigodev21@gmail.com>
2024-12-10 12:29:15 -0300

    aplicação pronta

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,22 @@ import metodos.Metodos;
 
 public class Main {
     public static void main(String[] args) {
-
         Metodos srv = new Metodos((String)null, (Double)null);
         Scanner tec = new Scanner(System.in);
+
         srv.inicio();
+
         System.out.println("qual serviço?".toUpperCase());
         int servico = tec.nextInt();
+
         switch (servico) {
             case 1:
                 srv.guardarNotas();
-                srv.verNota();
-                case 2:
-                    break;
-                    default:
-                        System.out.println("valor indefinido ou não encontrado");
-                }
-
-                tec.close();
-            }
+                srv.conf();
+                break;
+            default:
+                System.out.println("valor indefinido ou não encontrado");
         }
+                tec.close();
+    }
+}
--- src/metodos/Metodos.java
+++ src/metodos/Metodos.java
@@ -7,7 +7,7 @@ public class Metodos extends Thread {
     Scanner tec;
     private String nome;
     private Double nota;
-    ArrayList<Double> notas;
+    private ArrayList<Double> notas;
 
     public Metodos(String nome, Double nota) {
         this.tec = new Scanner(System.in);
@@ -19,17 +19,16 @@ public class Metodos extends Thread {
     public void inicio() {
         String regex = "^[A-Za-z-]+$";
 
-
         while (true) {
             System.out.println("Digite seu nome: ".toUpperCase());
             this.nome = this.tec.nextLine();
 
             if (nome.matches(regex)){
-                System.out.println("ola"+ nome);
+                System.out.println("ola "+ nome);
                 break;
             } else if (nome.isBlank()) {
                 continue;
-            }else {
+            } else {
                 System.out.println("Invalido");
             }
 
@@ -41,40 +40,59 @@ public class Metodos extends Thread {
     }
 
     public void guardarNotas() {
-        int i = 0;
         System.out.println("Digite Suas notas \n\"caso desista teclue (q)".toUpperCase());
 
+        String regex = "^[A-Za-z-]+$";
 
+        int i = 0;
         while (i < 4) {
             String entrada = this.tec.nextLine();
             if (entrada.equals("q")) {
                 break;
+            }else if(entrada.matches(regex)){
+                continue;
             }
 
             try {
                 double numero = Double.parseDouble(entrada);
                 this.notas.add(numero);
+                ++i;
+                System.out.println("Notas:");
             } catch (NumberFormatException var6) {
                 System.out.println("Entrada inválida. Digite um número válido.");
             }
-
-            ++i;
-            System.out.println("Lista de números:");
 
             for (double numero : this.notas) {
                 System.out.println(numero);
             }
         }
-
     }
 
-    public void verNota() {
-        double soma = (double) 0.0F;
+    public void conf() {
+        System.out.println("suas notas" + notas);
+        System.out.println("confirmar notas teclue \"1\" para sim e \"2\" para não");
+        int ent = tec.nextInt();
 
-        for (Double notas : this.notas) {
-            soma += notas;
+        if (ent == 1){
+            double soma = 0.0;
+            for (Double numero : notas) {
+                soma += numero;
+            }
+
+            double resultado = soma / 4;
+
+            System.out.println("Valor Somado: " + String.format("%.2f", soma));
+            if (soma / 4 >= 7){
+                System.out.println("Parabéns " + nome + " você foi aprovado com a média  " + String.format("%.1f", resultado));
+            }else if (soma / 4 < 7){
+                System.out.println("sua nota foi " + resultado + " Infelizmente você não atingiu a média " + nome +
+                        " entre em contato com o seu professor para a recuperação");
+            }else{
+                System.out.println("não identificado");
+            }
+        } else if (ent == 2) {
+            guardarNotas();
         }
-
     }
 
     public String getNome() {
@@ -93,5 +111,4 @@ public class Metodos extends Thread {
         this.nota = nota;
     }
 
-    }
-
+}
